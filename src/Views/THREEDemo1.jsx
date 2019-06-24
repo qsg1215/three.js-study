233fd5c618929883896a0f785aa26972f6368827
huaqiao.chen <huaqiao.chen@biz-united.com.cn>
2019-06-24 15:22:22 +0800

    some change

--- src/Views/THREEDemo1.jsx
+++ src/Views/THREEDemo1.jsx
@@ -1,19 +1,14 @@
+import React, {Component} from 'react';
+import * as THREE from 'three';
 
-import React, { Component } from 'react'
-import * as THREE from 'three'
+import Line from './Three/line.jsx';
 
-import Line from './Three/line.jsx'
-import Index from './Three/入门.jsx'
-
-import './index.less'
-import './test.css'
-import '../../src/styles/global.less'
 export default class THREEDemo1 extends Component {
-    render() {
-        return <div  >Hello,APP
-            <Line></Line>
-            <Index></Index>
-        </div>;
-    }
-
+  render() {
+    return (
+      <div>
+        <Line />
+      </div>
+    );
+  }
 }
