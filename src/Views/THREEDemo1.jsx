c5dfa1fed8e8ab60430b7e361e44d95122348ad5
huaqiao.chen <huaqiao.chen@biz-united.com.cn>
2019-09-28 10:06:54 +0800

    file change

--- src/Views/THREEDemo1.jsx
+++ src/Views/THREEDemo1.jsx
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import Test from './Test/index.jsx'
 
+
+
 export default class THREEDemo1 extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    }
+  }
+
+  callbacks(val) {
+    console.log('=====> ', val)
+    if (val.data && val.data.type && val.data.type === 'answerResult') {
+      let data = JSON.parse(val.data.data)
+      this.setState({
+        name: data.name
+      })
+
+
+    }
+
+  }
+
+
+  componentDidMount() {
+    window.addEventListener('message', this.callbacks.bind(this))
+
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('message', this.callbacks.bind(this))
+  }
   render() {
     return (
       <div>
-        <div>测试</div>
+        <div>{this.state.name}</div>
         <Test></Test>
       </div>
     );
   }
 
-  componentDidMount() {
 
-  }
 
 }
